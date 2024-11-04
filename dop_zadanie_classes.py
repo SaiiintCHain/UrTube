adf6d053da087a927a7b94c0b61dae1632188685
SaiiintCHain <zok-hor@mail.ru>
2024-11-04 17:37:36 +0300

    Update dop_zadanie_classes.py

---
+++ dop_zadanie_classes.py
@@ -0,0 +1,114 @@
+from idlelib.editor import keynames
+import time
+
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = password
+        self.age = age
+
+    def __hash__(self):
+        return hash(self.password)
+
+    def __str__(self):
+        return self.nickname
+
+class Video:
+    time_now = 0
+    adult_mode = False
+    def __init__(self, title, duration , adult_mode = False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+class UrTube():
+    def __init__(self):
+        self.users = {}
+        self.videos = {}
+        self.current_user = None
+
+    def register(self,nickname, password, age):
+        if nickname  in self.users:
+            print(f'Пользователь {nickname} уже существует')
+        else:
+            self.users[nickname]=User(nickname, password, age)
+            print(f'Вы успешно зарегистрированы под именем {nickname}. Вход в аккаунт осуществлён!')
+            self.current_user = self.users[nickname]
+
+
+    def log_in(self,nickname, password):
+        if nickname in self.users:
+            if self.users[nickname].password == hash(password):
+                self.current_user = self.users[nickname]
+                print (f'Вы вошли под именем {nickname}')
+        else:
+            print(f'Пользователь {nickname} не найден')
+
+    def log_out(self):
+        self.current_user = None
+        print('Вы вышли из аккаунта')
+
+    def add(self, *videos):
+        for video in videos:
+            if video.title in self.videos:
+                print(f'Видео {video.title} уже существует')
+            else:
+                self.videos[video.title] = video
+
+    def get_videos(self, search_term):
+        found_videos = [title for title in self.videos if search_term.lower() in title.lower()]
+        if not found_videos:
+            print('Видео не найдено')
+        return found_videos
+
+    def watch_video(self, title):
+        if not self.current_user:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+            return
+
+        if title not in self.videos:
+            print(f'Видео "{title}" не найдено')
+            return
+
+        video = self.videos[title]
+        if video.adult_mode and self.current_user.age < 18:
+            print("Вам нет 18 лет, пожалуйста покиньте страницу")
+            return
+
+        print(f'Начало просмотра видео: "{video.title}"')
+        for i in range(1, video.duration + 1):
+            print(i, end=' ', flush=True)  # flush=True для немедленного вывода
+            time.sleep(1)
+        print('\nКонец видео')
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+# Добавление видео
+ur.add(v1, v2)
+
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
+
+
+
+
+
